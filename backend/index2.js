4eff5d393caaeaedba88c3ee5da521899e2d3929
Nageshwer Bantu <nbantu2011@gmail.com>
2024-04-09 18:55:21 -0400

    second change

--- backend/index2.js
+++ backend/index2.js
@@ -188,7 +188,7 @@ app.get("/gettrans", (req, res) => {
 
  mongoose.connect(process.env.MONGO_URI)
  .then( () => {
-     app.listen(process.env.PORT, () => {
+     app.listen(process.env.PORT || 4000, () => {
         console.log("Connected to DB and Server started on port 3260", process.env.PORT)
  })
 })
